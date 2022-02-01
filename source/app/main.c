d9dd6210c042d9f61e3a6b4c7abfe2e13d7d2b7f
675414668 <675414668@qq.com>
2022-02-02 01:23:32 +0800

    feat(version):增加软硬件版本号以及打印

--- source/app/main.c
+++ source/app/main.c
@@ -1,4 +1,5 @@
 #include "hal.h"
+#include "version.h"
 #include "task_lcd.h"
 
 int main(void)
@@ -9,7 +10,9 @@ int main(void)
 	hal_led_init();
 	hal_key_init();
 	task_lcd_init();
-
+	
+  printf("HARDWARE_VERSION:%s\r\n",HARDWARE_VERSION);
+	printf("SOFTWARE_VERSION:%s\r\n",SOFTWARE_VERSION);
     while(1)
 		{
 			task_lcd_display();
